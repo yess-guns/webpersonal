29d5f666248ad2b3a520f5c2c1b9f7ffab4c3179
yess-guns <yessy12384@gmail.com>
2020-05-16 18:03:04 -0500

    Se agrego el modelo portfolio y se añadio al panel de administración

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
