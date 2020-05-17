6893c6dc85d86cb6df3dace4a0f611da87ae3856
yess-guns <yessy12384@gmail.com>
2020-05-16 19:34:42 -0500

    Se soluciono el problema para añadir enlaces a los proyectos

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,8 +7,10 @@ class Project(models.Model):
         verbose_name="Título")
     description = models.TextField(
         verbose_name="Descripción")
-    image = models.ImageField(upload_to="projects",
+    image = models.ImageField(upload_to="projects",  
         verbose_name="Imagen")
+    link = models.URLField(null=True, blank=True,  # <=====
+        verbose_name="Dirección Web")
     created = models.DateTimeField(auto_now_add=True, 
         verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True,
