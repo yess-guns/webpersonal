73e788ec7281b2a863ae5d382f6714122d220002
yess-guns <yessy12384@gmail.com>
2020-05-16 19:01:06 -0500

    Se personalizo el admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Project
 
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
 # Register your models here.
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
